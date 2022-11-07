6f1ccad1a7ee560fa73d31e0d77b44d9a7e08b4d
oswinso <oswinso@gmail.com>
2022-11-07 14:53:42 -0500

    Create two_arms.py

--- envs/sim_example.py
+++ envs/sim_example.py
@@ -5,14 +5,17 @@ import numpy as np
 Minimal example for pile simulation. 
 """
 
-from carrot_sim import CarrotSim
+# from carrot_sim import CarrotSim
+from two_arms import TwoArmSim
 
-sim = CarrotSim()  # initialize sim.
+
+# sim = CarrotSim()  # initialize sim.
+sim = TwoArmSim()  # initialize sim.
 count = 0
 
 while True:
     # compute random actions.
-    u = -0.5 + 1.0 * np.random.rand(4)
+    u = -0.5 + 1.0 * np.random.rand(8)
     sim.update(u)
 
     # save screenshot
